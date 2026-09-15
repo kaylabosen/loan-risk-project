4f73af126292455cbd0ca4d83815ce49bd250f46
Kayla Bosen <kaylabosen@gmail.com>
2026-09-15 12:17:44 -0500

    Create loan payment calculator

--- loan_calculator.py
+++ loan_calculator.py
@@ -10,22 +10,7 @@ monthly_payment = (
     / (1 - (1 + monthly_rate) ** (-number_of_payments))
 )
 
-total_paid = monthly_payment * number_of_payments
-total_interest = total_paid - loan_amount
-
-total_monthly_debt = monthly_payment + other_monthly_debt
-dti = total_monthly_debt / monthly_income
-
-if dti < 0.36:
-    risk = "Lower Risk"
-elif dti < 0.50:
-    risk = "Moderate Risk"
-else:
-    risk = "Higher Risk"
-
 print("Loan Amount: $", loan_amount)
 print("Monthly Payment: $", round(monthly_payment, 2))
-print("Total Interest Paid: $", round(total_interest, 2))
-print("Debt-to-Income Ratio:", round(dti * 100, 2), "%")
-print("Risk Classification:", risk)
+
 
