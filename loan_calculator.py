42fae8be753dcc8d41e518e6cb19259bf5f8cf71
Kayla Bosen <kaylabosen@gmail.com>
2026-09-15 12:22:59 -0500

    Add debt-to-income risk classification

--- loan_calculator.py
+++ loan_calculator.py
@@ -2,6 +2,9 @@ loan_amount = 20000
 annual_rate = 0.06
 years = 5
 
+monthly_income = 4000
+other_monthly_debt = 900
+
 monthly_rate = annual_rate / 12
 number_of_payments = years * 12
 
@@ -13,9 +16,21 @@ monthly_payment = (
 total_paid = monthly_payment * number_of_payments
 total_interest = total_paid - loan_amount
 
+total_monthly_debt = monthly_payment + other_monthly_debt
+dti = total_monthly_debt / monthly_income
+
+if dti < 0.36:
+    risk = "Lower Risk"
+elif dti < 0.50:
+    risk = "Moderate Risk"
+else:
+    risk = "Higher Risk"
+
 print("Loan Amount: $", loan_amount)
 print("Monthly Payment: $", round(monthly_payment, 2))
 print("Total Interest Paid: $", round(total_interest, 2))
+print("Debt-to-Income Ratio:", round(dti * 100, 2), "%")
+print("Risk Classification:", risk)
 
 
 
